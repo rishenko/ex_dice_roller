27cd4f717f40e716d563205769a5d79614c60a7e
Kevin McAbee <kevin.mcabee@gmail.com>
2018-09-24 14:53:05 -0700

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DiceRoller.MixProject do
   def project do
     [
       app: :dice_roller,
-      version: "0.2.0",
+      version: "0.2.0-alpha",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
