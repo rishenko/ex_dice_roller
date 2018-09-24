a1c087bdb665784157179fa9f3279746d4f33ad7
Kevin McAbee <kevin.mcabee@gmail.com>
2018-09-24 14:32:01 -0700

    added coverage and analysis libraries, prepping for hex publishing

--- mix.exs
+++ mix.exs
@@ -4,23 +4,46 @@ defmodule DiceRoller.MixProject do
   def project do
     [
       app: :dice_roller,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      name: "DiceRoller",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  def description do
+    "Adds simple or complex dice roll calculations to your application."
+  end
+
+  def package do
+    [
+      maintainers: ["Kevin McAbee"],
+      licenses: ["MIT"],
+      homepage_url: "https://github.com/rishenko/dice_roller",
+      source_url: "https://github.com/rishenko/dice_roller"
+    ]
+  end
+
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:excoveralls, "~> 0.10", only: [:test]},
+      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.18.0", only: :dev, runtime: false}
     ]
   end
